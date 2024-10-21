5a2b68d784004cec6a813570ee5ef372c20f2197
RudyChan <yanruchen000@gmail.com>
2024-10-21 14:52:27 +0900

    Exception Handling done

---
+++ TMBackend/src/main/java/com/rudy/TMBackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.rudy.TMBackend.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), "Resource Not Found");
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> badRequestException(BadRequestException ex) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), "Bad Request");
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<?> unauthorizedException(UnauthorizedException ex) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), "Unauthorized");
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    // ErrorDetails class
+    static class ErrorDetails {
+        private Date timestamp;
+        private String message;
+        private String details;
+
+        public ErrorDetails(Date timestamp, String message, String details) {
+            super();
+            this.timestamp = timestamp;
+            this.message = message;
+            this.details = details;
+        }
+
+        // Getters and Setters
+        public Date getTimestamp() {
+            return timestamp;
+        }
+        public void setTimestamp(Date timestamp) {
+            this.timestamp = timestamp;
+        }
+        public String getMessage() {
+            return message;
+        }
+        public void setMessage(String message) {
+            this.message = message;
+        }
+        public String getDetails() {
+            return details;
+        }
+        public void setDetails(String details) {
+            this.details = details;
+        }
+    }
+}
---
+++ TMBackend/src/main/java/com/rudy/TMBackend/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rudy.TMBackend.exception;
+
+//Create Custom Exceptions
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
