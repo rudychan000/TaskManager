3c0355c28facad0025ff62a87cf46af1a8c0f554
RudyChan <yanruchen000@gmail.com>
2024-10-27 18:00:15 +0900

    rename and add notfound

--- tm_frontend/src/pages/NotFoundPage.js
+++ tm_frontend/src/pages/NotFoundPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NotFoundPage.css'; // Optional: if you want to add some custom styles
+//import './NotFoundPage.css'; // Optional: if you want to add some custom styles
 
 const NotFoundPage = () => {
     return (
